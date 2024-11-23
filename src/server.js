5ace5681ae7b15a15db9b16ff9ae8c4fd88854b2
DrStone4384 <135196724+DrStone438@users.noreply.github.com>
2024-11-23 20:30:52 -0300

    ahora renderiza la pagina principal

--- src/server.js
+++ src/server.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const { create } = require('express-handlebars');
 const path = require('path');
 // Initializations
 const app = express();
 
 // Settings
 app.set('port', process.env.PORT || 4000);
-app.set('views', path.join(__dirname + 'views'));
+app.set('views', path.join(__dirname , 'views'));
+app.use(express.static(path.join(__dirname, "public")));
+
+const hbs = create({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+});
+app.engine('.hbs', hbs.engine); // Usa el motor correctamente
+app.set('view engine', '.hbs');
 
 // Middlewares
 app.use(express.urlencoded({extended: false}));
@@ -15,7 +26,7 @@ app.use(express.urlencoded({extended: false}));
 
 // Routes
 app.get('/', (req, res) => {
-    res.send('hello world')
+    res.render('index')
 });
 
 // Static files
