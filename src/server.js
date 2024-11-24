ef44d3b82df723ad2877d9cda30b9986a59e5fa6
DrStone4384 <135196724+DrStone438@users.noreply.github.com>
2024-11-24 00:27:16 -0300

    apunto de que funcione el formulario

--- src/server.js
+++ src/server.js
@@ -27,6 +27,7 @@ app.use(express.urlencoded({extended: false}));
 
 // Routes
 app.use(require('./routes/index.routes'));
+app.use(require('./routes/users.routes'));
 
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
